f9df5663c5e6b6b82d262f7643293294c21c9662
Lor92500 <149686206+Lor92500@users.noreply.github.com>
2024-02-14 10:54:35 +0100

    Challenge of inserting user into the database is over

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -1,6 +1,6 @@
 const database = require("../../databases");
 
-// Obtenez tous les utilisateurs
+
 const getUsers = (req, res) => {
   database
     .query("SELECT * FROM users")
@@ -13,7 +13,7 @@ const getUsers = (req, res) => {
     });
 };
 
-// Obtenez un utilisateur par son ID
+
 const getUserById = (req, res) => {
   const id = parseInt(req.params.id);
 
@@ -23,7 +23,7 @@ const getUserById = (req, res) => {
       if (users.length > 0) {
         res.json(users[0]);
       } else {
-        res.sendStatus(404); // Utilisateur non trouvé
+        res.sendStatus(404);
       }
     })
     .catch((err) => {
@@ -32,7 +32,25 @@ const getUserById = (req, res) => {
     });
 };
 
+const postUser = (req, res) => {
+    const { firstname, lastname, email, city, language } = req.body;
+  
+    database
+      .query(
+        "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+        [firstname, lastname, email, city, language]
+      )
+      .then(([result]) => {
+        res.status(201).send({ id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+
 module.exports = {
   getUsers,
   getUserById,
+  postUser,
 };
