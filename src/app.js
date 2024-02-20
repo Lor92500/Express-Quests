2d7ec81dd34ab4f6b148bb6543185abb2fa672c3
Lor92500 <149686206+Lor92500@users.noreply.github.com>
2024-02-20 17:25:12 +0100

    Route delete data

--- src/app.js
+++ src/app.js
@@ -14,13 +14,15 @@ const validateUser = require("./middlewares/validateUser");
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.post("/api/movies", validateMovie, movieControllers.postMovie); 
-app.put("/api/movies/:id", validateMovie, movieControllers.putMovie); 
+app.put("/api/movies/:id", validateMovie, movieControllers.putMovie);
+app.delete("/api/movies/:id", movieControllers.deleteMovie); 
 
 //users
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
 app.post("/api/users", validateUser, userControllers.postUser); 
 app.put("/api/users/:id", validateUser, userControllers.putUser); 
+app.delete("/api/users/:id", userControllers.deleteUser);
 
 module.exports = app;
 
